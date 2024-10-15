98907aa435f6b0968c089f2291070b88ef266260
tarunrajput <tarunrajput1337@gmail.com>
2024-10-15 14:17:52 +0530

    feat: add list pull request fn

--- pull.go
+++ pull.go
@@ -5,12 +5,14 @@
 package gogs
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"time"
 )
 
-// PullRequest represents a pull reqesut API object.
+// PullRequest represents a pull request API object.
 type PullRequest struct {
-	// Copied from issue.go
 	ID        int64      `json:"id"`
 	Index     int64      `json:"number"`
 	Poster    *User      `json:"user"`
@@ -22,16 +24,46 @@ type PullRequest struct {
 	State     StateType  `json:"state"`
 	Comments  int        `json:"comments"`
 
-	HeadBranch string      `json:"head_branch"`
-	HeadRepo   *Repository `json:"head_repo"`
-	BaseBranch string      `json:"base_branch"`
-	BaseRepo   *Repository `json:"base_repo"`
+	HTMLURL  string `json:"html_url"`
+	DiffURL  string `json:"diff_url"`
+	PatchURL string `json:"patch_url"`
 
-	HTMLURL string `json:"html_url"`
-
-	Mergeable      *bool      `json:"mergeable"`
+	Mergeable      bool       `json:"mergeable"`
 	HasMerged      bool       `json:"merged"`
 	Merged         *time.Time `json:"merged_at"`
 	MergedCommitID *string    `json:"merge_commit_sha"`
 	MergedBy       *User      `json:"merged_by"`
+
+	Base      *PRBranchInfo `json:"base"`
+	Head      *PRBranchInfo `json:"head"`
+	MergeBase string        `json:"merge_base"`
 }
+
+type PRBranchInfo struct {
+	Name       string      `json:"label"`
+	Ref        string      `json:"ref"`
+	Sha        string      `json:"sha"`
+	RepoID     int64       `json:"repo_id"`
+	Repository *Repository `json:"repo"`
+}
+
+type ListPullRequestsOptions struct {
+	Page  int    `json:"page"`
+	State string `json:"state"`
+}
+
+func (c *Client) ListRepoPullRequests(owner, repo string, opt ListPullRequestsOptions) ([]*PullRequest, error) {
+	body, err := json.Marshal(&opt)
+	if err != nil {
+		return nil, err
+	}
+	prs := make([]*PullRequest, 0, 10)
+	return prs, c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/pulls", owner, repo), jsonHeader, bytes.NewReader(body), &prs)
+}
+
+func (c *Client) GetPullRequest(owner, repo string, index int64) (*PullRequest, error) {
+	pr := new(PullRequest)
+	return pr, c.getParsedResponse("GET", fmt.Sprintf("/repos/%s/%s/pulls/%d", owner, repo, index), nil, nil, pr)
+}
+
+
